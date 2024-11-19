a398fcce2f969977c0ff20730d78a59037748da2
Britnay Gore <britnaygore268@gmail.com>
2024-11-18 20:32:46 -0500

    Added code to aid in creation of database in Program.cs file

--- OutTheGC/Program.cs
+++ OutTheGC/Program.cs
@@ -1,7 +1,23 @@
+using OutTheGC.Data;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// allows passing datetimes without time zone data 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// allows our api endpoints to access the database through Entity Framework Core
+builder.Services.AddNpgsql<OutTheGCDbContext>(builder.Configuration["OutTheGCDbConnectionString"]);
+
+// Set the JSON serializer options
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 var app = builder.Build();
 
