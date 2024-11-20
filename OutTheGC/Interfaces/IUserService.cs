84e7d2608a239795460393edf40d24af2a9f106b
Britnay Gore <britnaygore268@gmail.com>
2024-11-19 19:32:24 -0500

    Created interface for user to define the methods that the repository class must implement

---
+++ OutTheGC/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OutTheGC.Models;
+
+namespace OutTheGC.Interfaces;
+
+public interface IUserService
+{
+	Task<User> GetSingleUserAsync(Guid id);
+
+    Task<User> CheckUserExistenceAsync(string uid);
+
+    Task<User> CreateUserAsync(User newUser);
+
+    Task<User> UpdateUserAsync(Guid id, User existingUser);
+
+    Task<User> DeleteUserAsync(Guid id);
+}
+
