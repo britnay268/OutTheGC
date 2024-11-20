9b52fd88bc43de2b79847fe09dc8e98d2ec6153a
Britnay Gore <britnaygore268@gmail.com>
2024-11-19 19:45:33 -0500

    Created repository for user data

--- OutTheGC/Interfaces/IUserService.cs
+++ OutTheGC/Interfaces/IUserService.cs
@@ -15,4 +15,3 @@ public interface IUserService
 
     Task<User> DeleteUserAsync(Guid id);
 }
-
---
+++ OutTheGC/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using OutTheGC.Models;
+using Microsoft.EntityFrameworkCore;
+using OutTheGC.Interfaces;
+using OutTheGC.Data;
+
+namespace OutTheGC.Repositories;
+
+public class UserRepository : IUserRepository
+{
+	private readonly OutTheGCDbContext dbContext;
+
+	public UserRepository(OutTheGCDbContext context)
+	{
+		dbContext = context;
+	}
+
+	public async Task<User> GetSingleUserAsync(Guid id)
+	{
+		throw new NotImplementedException();
+	}
+
+    public async Task<User> CheckUserExistenceAsync(string uid)
+	{
+        throw new NotImplementedException();
+    }
+
+    public async Task<User> CreateUserAsync(User newUser)
+	{
+        throw new NotImplementedException();
+    }
+
+    public async Task<User> UpdateUserAsync(Guid id, User existingUser)
+	{
+        throw new NotImplementedException();
+    }
+
+    public async Task<User> DeleteUserAsync(Guid id)
+	{
+        throw new NotImplementedException();
+    }
+}
+
