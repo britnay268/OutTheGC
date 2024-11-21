6301fb59dfb6d0e6948ca0d7703adca7bbaaa1a5
Britnay Gore <britnaygore268@gmail.com>
2024-11-21 00:08:50 -0500

    Added methods to the trip repository for data access without doing data access

--- OutTheGC/Repositories/TripRepository.cs
+++ OutTheGC/Repositories/TripRepository.cs
@@ -1,11 +1,47 @@
 ﻿using System;
-namespace OutTheGC.Repositories
+using OutTheGC.Interfaces;
+using OutTheGC.Data;
+using OutTheGC.Models;
+
+namespace OutTheGC.Repositories;
+
+public class TripRepository : ITripRepository
 {
-	public class TripRepository
+	private readonly OutTheGCDbContext dbContext;
+
+	public TripRepository(OutTheGCDbContext context)
 	{
-		public TripRepository()
-		{
-		}
+		dbContext = context;
 	}
+
+    public async Task<List<Trip>> GetTripsAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Trip> GetTripsByIdAsync(Guid tripId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Trip> CreateTripAsync(Trip newTrip)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async  Task<Trip> UpdateTripAsync(Guid tripId, Trip updatedTrip)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Trip> DeleteTripAsync(Guid tripId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Trip> DeleteUserFromTripAsync(Guid tripId, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
 }
 
