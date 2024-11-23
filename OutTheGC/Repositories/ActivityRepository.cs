6d1c3727937e20eb7d7bcaf956862d47325ffaf8
Britnay Gore <britnaygore268@gmail.com>
2024-11-23 11:01:38 -0500

    Added methods to the repository class with functionality and the service class for activity

--- OutTheGC/Repositories/ActivityRepository.cs
+++ OutTheGC/Repositories/ActivityRepository.cs
@@ -1,11 +1,46 @@
-﻿using System;
-namespace OutTheGC.Repositories
+﻿using OutTheGC.Interfaces;
+using OutTheGC.Models;
+using OutTheGC.Data;
+
+namespace OutTheGC.Repositories;
+
+public class ActivityRepository : IActivityRepository
 {
-	public class ActivityRepository
+	private readonly OutTheGCDbContext dbContext;
+
+	public ActivityRepository(OutTheGCDbContext context)
 	{
-		public ActivityRepository()
-		{
-		}
+		dbContext = context;
 	}
+
+    public async Task<List<Activity>> GetActivitiesAsync(Guid tripId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Activity> GetSingleActivityAsync(Guid activityId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Activity> CreateActivityAsync(Activity newActivity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Activity> UpdateActivityAsync(Guid activityId, Activity updatedActivity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Activity> DeleteActivityAsync(Guid activityId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Activity>> SearchActivityAsync(Guid tripId, string searchInput)
+    {
+        throw new NotImplementedException();
+    }
 }
 
--- OutTheGC/Services/ActivityService.cs
+++ OutTheGC/Services/ActivityService.cs
@@ -1,11 +1,47 @@
-﻿using System;
-namespace OutTheGC.Services
+﻿using OutTheGC.Interfaces;
+using OutTheGC.Models;
+using OutTheGC.Data;
+using OutTheGC.Services;
+
+namespace OutTheGC.Services;
+
+public class ActivityService : IActivityService
 {
-	public class ActivityService
+	private readonly IActivityRepository _activityRepository;
+
+	public ActivityService(IActivityRepository activityRepository)
 	{
-		public ActivityService()
-		{
-		}
+		_activityRepository = activityRepository;
 	}
+
+    public async Task<List<Activity>> GetActivitiesAsync(Guid tripId)
+    {
+        return await _activityRepository.GetActivitiesAsync(tripId);
+    }
+
+    public async Task<Activity> GetSingleActivityAsync(Guid activityId)
+    {
+        return await _activityRepository.GetSingleActivityAsync(activityId);
+    }
+
+    public async Task<Activity> CreateActivityAsync(Activity newActivity)
+    {
+        return await _activityRepository.CreateActivityAsync(newActivity);
+    }
+
+    public async Task<Activity> UpdateActivityAsync(Guid activityId, Activity updatedActivity)
+    {
+        return await _activityRepository.UpdateActivityAsync(activityId, updatedActivity);
+    }
+
+    public async Task<Activity> DeleteActivityAsync(Guid activityId)
+    {
+        return await _activityRepository.DeleteActivityAsync(activityId);
+    }
+
+    public async Task<List<Activity>> SearchActivityAsync(Guid tripId, string searchInput)
+    {
+        return await _activityRepository.SearchActivityAsync(tripId, searchInput);
+    }
 }
 
